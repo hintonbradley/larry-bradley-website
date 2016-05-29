89313b875cd4382a831e9eb12f1c859694a730e6
James Bradley <hintonbradley@gmail.com>
2016-05-28 23:54:41 -0700

    Rework main and mailboxes pages

--- app/controllers/mailboxes-controller.js
+++ app/controllers/mailboxes-controller.js
@@ -2,6 +2,31 @@
 	//We're adding data to the angular module so it must begin with angular.module
 	angular.module('LarryBradley')
 	.controller('MailboxesController', ['$scope','$state','$http',function($scope, $state, $http) {
+		
+		$scope.myInterval = 5000;
+		  var siding = $scope.siding = [
+			  {
+			  	"image":"app/images/mbx1.jpg","active":true
+			  },
+			  {
+			  	"image":"app/images/mbx2.jpg","active":false
+			  },
+			  {
+			  	"image":"app/images/mbx3.jpg","active":false
+			  },
+			  ];
+
+		$scope.myInterval = 5000;
+		  var brick = $scope.brick = [
+			  {
+			  	"image":"app/images/mbx2.1.jpg","active":true
+			  },
+			  {
+			  	"image":"app/images/mbx2.2.jpg","active":false
+			  }
+			  ];
+
+
 		$scope.sendComment = function(event) {
 			var request = {
 				name: $scope.newComment.name,
---
+++ app/controllers/slideshow-controller.js
@@ -0,0 +1,6 @@
+(function() {
+	//We're adding data to the angular module so it must begin with angular.module
+		angular.module('LarryBradley')
+		.controller('SlideshowController', ['$scope','$state','$log', function($scope, $state, $log) {
+		}]);
+	}());

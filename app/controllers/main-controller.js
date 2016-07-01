dfcf949334fba862f2c635e8c106461c016738c7
James Bradley <hintonbradley@gmail.com>
2016-07-01 11:37:50 -0700

    updated styling and controllers

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -3,34 +3,34 @@
 		angular.module('LarryBradley')
 		.controller('MainController', ['$scope','$state','$log', function($scope, $state, $log) {
 
-			  $scope.status = {
-			    isopen: false
-			  };
+			  // $scope.status = {
+			  //   isopen: false
+			  // };
 
-			  $scope.toggled = function(open) {
-			    $log.log('Dropdown is now: ', open);
-			  };
+			  // $scope.toggled = function(open) {
+			  //   $log.log('Dropdown is now: ', open);
+			  // };
 
-			  $scope.toggleDropdown = function($event) {
-			    $event.preventDefault();
-			    $event.stopPropagation();
-			    $scope.status.isopen = !$scope.status.isopen;
-			  };
+			  // $scope.toggleDropdown = function($event) {
+			  //   $event.preventDefault();
+			  //   $event.stopPropagation();
+			  //   $scope.status.isopen = !$scope.status.isopen;
+			  // };
 
-			  $scope.myInterval = 5000;
-			  var slides = $scope.slides = [
-				  {
-				  	"image":"app/images/garage/garage.jpg","text":"My Garage","state":"garage", "active":true
-				  },
-				  {
-				  	"image":"app/images/mailboxes/mbx3.jpg","text":"Mailboxes","state":"mailboxes","active":false
-				  },
-				  {
-				  	"image":"app/images/mini-homes/gnomehome.jpg","text":"Miniature Houses","state":"minihouses","active":false
-				  },
-				  {
-				  	"image":"app/images/birdhouses/birdhouse1.png","text":"Birdhouses","state":"birdhouses","active":false
-				  }
-				  ];
+			  // $scope.myInterval = 5000;
+			  // var slides = $scope.slides = [
+				 //  {
+				 //  	"image":"app/images/garage/garage.jpg","text":"My Garage","state":"garage", "active":true
+				 //  },
+				 //  {
+				 //  	"image":"app/images/mailboxes/mbx3.jpg","text":"Mailboxes","state":"mailboxes","active":false
+				 //  },
+				 //  {
+				 //  	"image":"app/images/mini-homes/gnomehome.jpg","text":"Miniature Houses","state":"minihouses","active":false
+				 //  },
+				 //  {
+				 //  	"image":"app/images/birdhouses/birdhouse1.png","text":"Birdhouses","state":"birdhouses","active":false
+				 //  }
+				 //  ];
 		}]);
 	}());

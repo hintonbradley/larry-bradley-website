b16b577b7ba845307fda9b086b1207edbd3e7011
James Bradley <hintonbradley@gmail.com>
2016-07-13 12:58:18 -0700

    updated post requests for project pages

--- server/controllers/comments-controller.js
+++ server/controllers/comments-controller.js
@@ -1,44 +1,85 @@
 // Requiring Comment model from models directory
 var Comment = require('../models/comments');
 
+// Old code (sync):
+// module.exports.postMailboxComment = function (req, res) {
+// 	var comment = new Comment(req.body);
+// 	comment.save();
+
+// 	Comment.find({"project":1})
+// 		.sort({date: -1}).exec(function(err, allComments){
+// 		if (err) {
+// 			res.error(error);
+// 		} else {
+// 			res.json(allComments);
+// 		}
+// 	});
+// }
+
+// New code (async):
 module.exports.postMailboxComment = function (req, res) {
 	var comment = new Comment(req.body);
 	comment.save();
-
-	Comment.find({"project":1})
-		.sort({date: -1}).exec(function(err, allComments){
-		if (err) {
-			res.error(error);
-		} else {
-			res.json(allComments);
+	.then(function(response){
+		if(response._id) {
+			Comment.find({"project":1})
+			.sort({date: -1})
+			.exec(function(err, allComments){
+				if (err) {
+					res.error(error);
+				} 
+				else {
+				res.json(allComments);
+				}
+			})
 		}
-	});
-}
+	})
+};
 
 module.exports.getMailboxComments = function (req, res) {
 	Comment.find({"project":1})
-		.sort({date: -1})
-		.exec(function(err, allComments) {
-			if (err) {
-				res.error(err)
-			} else {
-				res.json(allComments);
-			}
-		})
-}
-
-module.exports.postBirdhouseComment = function (req, res) {
-	var comment = new Comment(req.body);
-	comment.save();
-
-	Comment.find({"project":2})
-		.sort({date: -1}).exec(function(err, allComments){
+	.sort({date: -1})
+	.exec(function(err, allComments) {
 		if (err) {
-			res.error(error);
+			res.error(err)
 		} else {
 			res.json(allComments);
 		}
-	});
+	})
+}
+
+// Old Code (sync):
+// module.exports.postBirdhouseComment = function (req, res) {
+// 	var comment = new Comment(req.body);
+// 	comment.save();
+
+// 	Comment.find({"project":2})
+// 		.sort({date: -1}).exec(function(err, allComments){
+// 		if (err) {
+// 			res.error(error);
+// 		} else {
+// 			res.json(allComments);
+// 		}
+// 	});
+// }
+
+// New Code (async):
+module.exports.postBirdhouseComment = function (req, res) {
+	var comment = new Comment(req.body);
+	comment.save();
+	.then(function(response){
+		if(response._id) {
+			Comment.find({"project":2})
+				.sort({date: -1})
+				.exec(function(err, allComments){
+				if (err) {
+					res.error(error);
+				} else {
+					res.json(allComments);
+				}
+			});
+		}
+	})
 }
 
 // creating the function that will grab all the birdhouse comments from the database
@@ -46,30 +87,50 @@ module.exports.getBirdhouseComments = function (req, res) {
 	// grabbing all comments that have a project value of 2 (comments made on birdhouses page)
 	Comment.find({"project":2})
 	// sorting by date, descending
-		.sort({date: -1})
-		.exec(function(err, allComments) {
-			if (err) {
-				console.log ("there is a birdhouse err: ", err)
-				res.error(err)
-			} else {
-				console.log("all comments from the comments controller are: ", allComments);
-				res.json(allComments);
-			}
-		})
-}
-
-module.exports.postMinihouseComment = function (req, res) {
-	var comment = new Comment(req.body);
-	comment.save();
-
-	Comment.find({"project":3})
-		.sort({date: -1}).exec(function(err, allComments){
+	.sort({date: -1})
+	.exec(function(err, allComments) {
 		if (err) {
-			res.error(error);
+			console.log ("there is a birdhouse err: ", err)
+			res.error(err)
 		} else {
+			console.log("all comments from the comments controller are: ", allComments);
 			res.json(allComments);
 		}
-	});
+	})
+}
+
+// Old code (sync):
+// module.exports.postMinihouseComment = function (req, res) {
+// 	var comment = new Comment(req.body);
+// 	comment.save();
+
+// 	Comment.find({"project":3})
+// 		.sort({date: -1}).exec(function(err, allComments){
+// 		if (err) {
+// 			res.error(error);
+// 		} else {
+// 			res.json(allComments);
+// 		}
+// 	});
+// }
+
+// New code (async):
+module.exports.postMinihouseComment = function (req, res) {
+	var comment = new Comment(req.body);
+	comment.save()
+	.then(function(response){
+		if(response._id) {
+			Comment.find({"project":3})
+				.sort({date: -1})
+				.exec(function(err, allComments){
+				if (err) {
+					res.error(error);
+				} else {
+					res.json(allComments);
+				}
+			});
+		}
+	})
 }
 
 // creating the function that will grab all the minihouses comments from the database
@@ -77,16 +138,16 @@ module.exports.getMinihouseComments = function (req, res) {
 	// grabbing all comments that have a project value of 2 (comments made on birdhouses page)
 	Comment.find({"project":3})
 	// sorting by date, descending
-		.sort({date: -1})
-		.exec(function(err, allComments) {
-			if (err) {
-				console.log ("there is a minihouse err: ", err)
-				res.error(err)
-			} else {
-				console.log("all comments from the comments controller are: ", allComments);
-				res.json(allComments);
-			}
-		})
+	.sort({date: -1})
+	.exec(function(err, allComments) {
+		if (err) {
+			console.log ("there is a minihouse err: ", err)
+			res.error(err)
+		} else {
+			console.log("all comments from the comments controller are: ", allComments);
+			res.json(allComments);
+		}
+	})
 }
 
 // sync (old):
@@ -114,7 +175,8 @@ module.exports.postPethouseComment = function (req, res) {
 		console.log(response._id);
 		if(response._id) {
 			Comment.find({"project":4})
-				.sort({date: -1}).exec(function(err, allComments){
+				.sort({date: -1})
+				.exec(function(err, allComments){
 				if (err) {
 					res.error(error);
 				} else {
@@ -129,14 +191,14 @@ module.exports.getPethouseComments = function (req, res) {
 	// grabbing all comments that have a project value of 4 (comments made on birdhouses page)
 	Comment.find({"project":4})
 	// sorting by date, descending
-		.sort({date: -1})
-		.exec(function(err, allComments) {
-			if (err) {
-				console.log ("there is a pethouse err: ", err)
-				res.error(err)
-			} else {
-				console.log("all comments from the comments controller are: ", allComments);
-				res.json(allComments);
-			}
-		})
+	.sort({date: -1})
+	.exec(function(err, allComments) {
+		if (err) {
+			console.log ("there is a pethouse err: ", err)
+			res.error(err)
+		} else {
+			console.log("all comments from the comments controller are: ", allComments);
+			res.json(allComments);
+		}
+	})
 }
